181c23794505bdc077dc5155dfe677a457f028c3
Vivek Singh <viveksingh31722@gamil.com>
2024-05-15 11:34:40 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -11,6 +12,9 @@ mongoose.connect(process.env.MONGO).then(() =>{
 })
 
 const app = express()
+
+app.use(express.json());
+
 const port = 3000
 
 app.get('/test', (req, res) => {
@@ -24,3 +28,4 @@ app.listen(port, () => {
 })
 
 app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,16 +4,16 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
     
   }
 
