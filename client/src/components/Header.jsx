4fb2b91eeb0053eec0a0a4440fdc5d150817f8de
Vivek Singh <viveksingh31722@gamil.com>
2024-06-03 01:06:53 +0530

    Create search page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <h1 className='font-bold text-xl sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Sahand</span>
             <span className='text-slate-700'>Estate</span>
           </h1>
