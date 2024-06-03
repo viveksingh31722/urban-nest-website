48b4855767dce25ccbb9a1c829670553def9be13
Vivek Singh <viveksingh31722@gamil.com>
2024-06-03 23:21:43 +0530

    corrected the fix

--- api/index.js
+++ api/index.js
@@ -8,46 +8,39 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() =>{
-  console.log('Connected to MongoDB');
-}).catch((err) =>{
-  console.log(err);
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-const port = 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
-app.get('/test', (req, res) => {
-  res.json({
-    message: 'Hello World!',
-  })
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-app.use("/api/user", userRouter);
+app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
-//The show below is middleWare.
-
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
@@ -55,5 +48,4 @@ app.use((err, req, res, next) =>{
     statusCode,
     message,
   });
-
-})
+});
