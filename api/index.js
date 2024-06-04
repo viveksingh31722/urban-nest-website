62085cca50670f03eb8efb906c2923fdbfe7bd2b
Vivek Singh <viveksingh31722@gamil.com>
2024-06-05 00:18:38 +0530

    improved pre installation

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
+ const __dirname = path.resolve();
 
 const app = express();
 
@@ -33,11 +33,9 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+app.get('*', (req, res) =>{
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 })
 
 app.use((err, req, res, next) => {
